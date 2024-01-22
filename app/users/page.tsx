1c2de3abae2013354e222261432bf0949a6f4d70
Winston Tsui <ny1winston@gmail.com>
2024-01-21 23:13:25 -0500

    Data fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+// Here we define the shape of our user objects.
+interface User {
+  id: number;
+  name: string;
 }
 
-export default UsersPage
+const UsersPage = async () => {
+  /* Fetch function allows us to send http requests to the backend.
+  This returns a promise, so we have to wait to get the response.
+  It is a built-in javaScript function used to make HTTP requests.
+
+  One thing I can do is to not have a cache:
+   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  
+  Another is to keep data fresh for only a certain period of time.
+  */
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: {revalidate: 10},
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users:</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}> {user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
